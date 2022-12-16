f4c60ea244d497fc428f0388bc75c0fbe8e1ac84
Enrico Marchionni <enrico.marchionni@studio.unibo.it>
2022-12-16 19:41:14 +0100

    4-ExtensionMethods done

--- 4-ExtensionMethods/Complex.cs
+++ 4-ExtensionMethods/Complex.cs
@@ -5,8 +5,8 @@ namespace ExtensionMethods
     /// <inheritdoc cref="IComplex"/>
     public class Complex : IComplex
     {
-        private readonly double re;
-        private readonly double im;
+        private readonly double _re;
+        private readonly double _im;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Complex"/> class.
@@ -15,71 +15,50 @@ namespace ExtensionMethods
         /// <param name="im">the imaginary part.</param>
         public Complex(double re, double im)
         {
-            this.re = re;
-            this.im = im;
+            this._re = re;
+            this._im = im;
         }
 
         /// <inheritdoc cref="IComplex.Real"/>
-        public double Real
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Real => _re;
 
         /// <inheritdoc cref="IComplex.Imaginary"/>
-        public double Imaginary
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Imaginary => _im;
 
         /// <inheritdoc cref="IComplex.Modulus"/>
-        public double Modulus
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Modulus =>
+            Math.Sqrt(Math.Pow(Real, 2) + Math.Pow(Imaginary, 2));
 
         /// <inheritdoc cref="IComplex.Phase"/>
-        public double Phase
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Phase => Math.Atan2(Imaginary, Real);
 
         /// <inheritdoc cref="IComplex.ToString"/>
         public override string ToString()
         {
-            // TODO improve
-            return base.ToString();
+            if (Imaginary == 0.0) return Real.ToString();
+            var imAbs = Math.Abs(Imaginary);
+            var imValue = imAbs == 1.0 ? "" : imAbs.ToString();
+            string sign;
+            if (Real == 0d)
+            {
+                sign = Imaginary > 0 ? "" : "-";
+                return sign + "i" + imValue;
+            }
+
+            sign = Imaginary > 0 ? "+" : "-";
+            return $"{Real} {sign} i{imValue}";
         }
 
         /// <inheritdoc cref="IEquatable{T}.Equals(T)"/>
-        public bool Equals(IComplex other)
-        {
-            throw new System.NotImplementedException();
-        }
+        public bool Equals(IComplex other) =>
+            Real.Equals(other.Real) && Imaginary.Equals(other.Imaginary);
 
         /// <inheritdoc cref="object.Equals(object?)"/>
-        public override bool Equals(object obj)
-        {
-            // TODO improve
-            return base.Equals(obj);
-        }
+        public override bool Equals(object obj) =>
+            obj is Complex && Equals((Complex) obj);
 
         /// <inheritdoc cref="object.GetHashCode"/>
-        public override int GetHashCode()
-        {
-            // TODO improve
-            return base.GetHashCode();
-        }
+        public override int GetHashCode() =>
+            HashCode.Combine(Real, Imaginary);
     }
 }
